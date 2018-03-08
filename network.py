09591bc60f1836d27b3f87e7bdfcec06fa30e8ed
ZhangYu9 <1901727338@qq.com>
2018-03-08 19:32:52 +0800

    Version 1.1

--- network.py
+++ network.py
@@ -1,102 +1,23 @@
-# Implementing a one-layer Neural Network
-#---------------------------------------
-#
-# We will illustrate how to create a one hidden layer NN
-#
-# We will use the iris data for this exercise
-#
-# We will build a one-hidden layer neural network
-#  to predict the fourth attribute, Petal Width from
-#  the other three (Sepal length, Sepal width, Petal length).
-
-import matplotlib.pyplot as plt
-import numpy as np
 import tensorflow as tf
-from sklearn import datasets
-from tensorflow.python.framework import ops
-ops.reset_default_graph()
-
-iris = datasets.load_iris()
-x_vals = np.array([x[0:3] for x in iris.data])
-y_vals = np.array([x[3] for x in iris.data])
-
-# Create graph session 
+import numpy as np
 sess = tf.Session()
+data_size = 25
+conv_size = 5
+maxpool_size = 5
+stride_size = 1
+data_1d = np.random.normal(size=data_size)
 
-# make results reproducible
-seed = 2
-tf.set_random_seed(seed)
-np.random.seed(seed)  
+x_input_1d = tf.placeholder(dtype = tf.float32,shape = [data_size])
 
-# Split data into train/test = 80%/20%
-train_indices = np.random.choice(len(x_vals), round(len(x_vals)*0.8), replace=False)
-test_indices = np.array(list(set(range(len(x_vals))) - set(train_indices)))
-x_vals_train = x_vals[train_indices]
-x_vals_test = x_vals[test_indices]
-y_vals_train = y_vals[train_indices]
-y_vals_test = y_vals[test_indices]
+def conv_layer_1d(input_1d,myfilter,stride):
+    input_2d = tf.expand_dims(input_1d,0)
+    input_3d = tf.expand_dims(input_2d,0)
+    input_4d = tf.expand_dims(input_3d,3)
 
-# Normalize by column (min-max norm)
-def normalize_cols(m):
-    col_max = m.max(axis=0)
-    col_min = m.min(axis=0)
-    return (m-col_min) / (col_max - col_min)
-    
-x_vals_train = np.nan_to_num(normalize_cols(x_vals_train))
-x_vals_test = np.nan_to_num(normalize_cols(x_vals_test))
+    convolution_output = tf.nn.conv2d(input_4d,filter=my_filter,strides=[1,1,stride,1],padding="VALID")
+    conv_output_1d = tf.squeeze(convolution_output)
+    return(conv_output_1d)
 
-# Declare batch size
-batch_size = 50
+my_filter = tf.Variable(tf.random_normal(shape=[1,conv_size,1,1]))
+my_convolution_output = conv_layer_1d(x_input_1d, my_filter,stride=stride_size)
 
-# Initialize placeholders
-x_data = tf.placeholder(shape=[None, 3], dtype=tf.float32)
-y_target = tf.placeholder(shape=[None, 1], dtype=tf.float32)
-
-# Create variables for both NN layers
-hidden_layer_nodes = 10
-A1 = tf.Variable(tf.random_normal(shape=[3,hidden_layer_nodes])) # inputs -> hidden nodes
-b1 = tf.Variable(tf.random_normal(shape=[hidden_layer_nodes]))   # one biases for each hidden node
-A2 = tf.Variable(tf.random_normal(shape=[hidden_layer_nodes,1])) # hidden inputs -> 1 output
-b2 = tf.Variable(tf.random_normal(shape=[1]))   # 1 bias for the output
-
-# Declare model operations
-hidden_output = tf.nn.relu(tf.add(tf.matmul(x_data, A1), b1))
-final_output = tf.nn.relu(tf.add(tf.matmul(hidden_output, A2), b2))
-
-# Declare loss function (MSE)
-loss = tf.reduce_mean(tf.square(y_target - final_output))
-
-# Declare optimizer
-my_opt = tf.train.GradientDescentOptimizer(0.005)
-train_step = my_opt.minimize(loss)
-
-# Initialize variables
-init = tf.global_variables_initializer()
-sess.run(init)
-
-# Training loop
-loss_vec = []
-test_loss = []
-for i in range(500):
-    rand_index = np.random.choice(len(x_vals_train), size=batch_size)
-    rand_x = x_vals_train[rand_index]
-    rand_y = np.transpose([y_vals_train[rand_index]])
-    sess.run(train_step, feed_dict={x_data: rand_x, y_target: rand_y})
-
-    temp_loss = sess.run(loss, feed_dict={x_data: rand_x, y_target: rand_y})
-    loss_vec.append(np.sqrt(temp_loss))
-    
-    test_temp_loss = sess.run(loss, feed_dict={x_data: x_vals_test, y_target: np.transpose([y_vals_test])})
-    test_loss.append(np.sqrt(test_temp_loss))
-    if (i+1)%50==0:
-        print('Generation: ' + str(i+1) + '. Loss = ' + str(temp_loss))
-
-
-# Plot loss (MSE) over time
-plt.plot(loss_vec, 'k-', label='Train Loss')
-plt.plot(test_loss, 'r--', label='Test Loss')
-plt.title('Loss (MSE) per Generation')
-plt.legend(loc='upper right')
-plt.xlabel('Generation')
-plt.ylabel('Loss')
-plt.show()

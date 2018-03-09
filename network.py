cd2a3c0cc167b98689b8573f4a286515972910df
ZhangYu9 <1901727338@qq.com>
2018-03-09 14:57:55 +0800

    dev version 1.1

--- network.py
+++ network.py
@@ -1,23 +1,72 @@
-import tensorflow as tf
+#-*- coding:utf-8 -*-
+import matplotlib.pyplot as plt
 import numpy as np
+import tensorflow as tf
+from sklearn import datasets
+
+iris = datasets.load_iris()
+x_vals = np.array([x[0:3] for x in iris.data])
+y_vals = np.array([x[3] for x in iris.data])
+
 sess = tf.Session()
-data_size = 25
-conv_size = 5
-maxpool_size = 5
-stride_size = 1
-data_1d = np.random.normal(size=data_size)
+seed = 2
+tf.set_random_seed(seed)
+np.random.seed(seed)
+train_indices = np.random.choice(len(x_vals),round(len(x_vals)*0.8),replace=False)
+test_indices = np.array(list(set(range(len(x_vals)))-set(train_indices)))
 
-x_input_1d = tf.placeholder(dtype = tf.float32,shape = [data_size])
+x_vals_train = x_vals[train_indices]
+x_vals_test = x_vals[test_indices]
+y_vals_train = y_vals[train_indices]
+y_vals_test = y_vals[test_indices]
 
-def conv_layer_1d(input_1d,myfilter,stride):
-    input_2d = tf.expand_dims(input_1d,0)
-    input_3d = tf.expand_dims(input_2d,0)
-    input_4d = tf.expand_dims(input_3d,3)
+def normalize_cols(m):
+    col_max = m.max(axis=0)
+    col_min = m.min(axis=0)
+    return (m-col_min)/(col_max - col_min)
 
-    convolution_output = tf.nn.conv2d(input_4d,filter=my_filter,strides=[1,1,stride,1],padding="VALID")
-    conv_output_1d = tf.squeeze(convolution_output)
-    return(conv_output_1d)
+x_vals_train = np.nan_to_num(normalize_cols(x_vals_train))
+x_vals_test = np.nan_to_num(normalize_cols(x_vals_test))
 
-my_filter = tf.Variable(tf.random_normal(shape=[1,conv_size,1,1]))
-my_convolution_output = conv_layer_1d(x_input_1d, my_filter,stride=stride_size)
+batch_size = 50
+x_data = tf.placeholder(shape=[None,3],dtype=tf.float32)
+y_target = tf.placeholder(shape=[None,1],dtype=tf.float32)
 
+hidden_layer_nodes = 10
+
+A1 = tf.Variable(tf.random_normal(shape=[3,hidden_layer_nodes]))
+b1 = tf.Variable(tf.random_normal(shape=[hidden_layer_nodes]))
+A2 = tf.Variable(tf.random_normal(shape=[hidden_layer_nodes,1]))
+b2 = tf.Variable(tf.random_normal(shape=[1]))
+
+hidden_output = tf.nn.relu(tf.add(tf.matmul(x_data,A1),b1))
+final_output = tf.nn.relu(tf.add(tf.matmul(hidden_output,A2),b2))
+
+loss = tf.reduce_mean(tf.square(y_target-final_output))
+my_opt = tf.train.GradientDescentOptimizer(0.005)
+train_step = my_opt.minimize(loss)
+
+init = tf.global_variables_initializer()
+
+sess.run(init)
+loss_vec = []
+test_loss = []
+for i in range(500):
+    rand_index = np.random.choice(len(x_vals_train),size=batch_size)
+    rand_x = x_vals_train[rand_index]
+    rand_y = np.transpose([y_vals_train[rand_index]])
+    sess.run(train_step,feed_dict={x_data:rand_x,y_target:rand_y})
+    temp_loss = sess.run(loss,feed_dict={x_data:rand_x,y_target:rand_y})
+    loss_vec.append(np.sqrt(temp_loss))
+    test_temp_loss = sess.run(loss,feed_dict={x_data:x_vals_test,y_target:np.transpose([y_vals_test])})
+    test_loss.append(np.sqrt(test_temp_loss))
+    if (i+1)%50 == 0:
+        print('Generation:'+ str(i+1) + ' .Loss '+ str(temp_loss))
+
+plt.plot(loss_vec,'k-',label='Train Loss')
+plt.plot(test_loss,'r--',label='Test Loss')
+plt.title('Loss per Generation')
+plt.xlabel('Generation')
+plt.ylabel('Loss')
+plt.legend(loc='upper right')
+plt.show()
